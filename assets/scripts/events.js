bc67037c6710eeb172ae985d559fbfcd9144f946
ayoungdrew <andrewyoung504@gmail.com>
2018-02-09 10:32:40 -0500

    Refactor JS scripts
    Refactor JS scripts; index.js now a lot cleaner and logic.js contains
    game logic code

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,14 +1,12 @@
 'use strict'
 const api = require('./api')
 const getFormFields = require('../../lib/get-form-fields')
-const store = require('./store')
 const ui = require('./ui')
-// const ui = require('./ui')
+const logic = require('./logic')
 
-
-
-// HANDLERSSS!
+// EVENT HANDLERSSS!
 const addHandlers = () => {
+  // Control panel/user stuff; scroll down for gameplay handlers
   $('#sign-up').on('submit', function (event) {
     event.preventDefault()
     const data = getFormFields(this)
@@ -51,6 +49,16 @@ const addHandlers = () => {
       .then(ui.signOutSuccess)
       .catch(ui.signOutFailure)
   })
+  // Game board handlers
+  $('#first').on('click', function () { logic.fieldPickLogic('#first', 0) })
+  $('#second').on('click', function () { logic.fieldPickLogic('#second', 1) })
+  $('#third').on('click', function () { logic.fieldPickLogic('#third', 2) })
+  $('#fourth').on('click', function () { logic.fieldPickLogic('#fourth', 3) })
+  $('#fifth').on('click', function () { logic.fieldPickLogic('#fifth', 4) })
+  $('#sixth').on('click', function () { logic.fieldPickLogic('#sixth', 5) })
+  $('#seventh').on('click', function () { logic.fieldPickLogic('#seventh', 6) })
+  $('#eighth').on('click', function () { logic.fieldPickLogic('#eighth', 7) })
+  $('#ninth').on('click', function () { logic.fieldPickLogic('#ninth', 8) })
 }
 
 module.exports = {
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,7 +3,6 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvent = require('./events')
-const store = require('./store')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -12,65 +11,3 @@ $(() => {
   //   console.log('hey!')
   // })
 })
-
-const gameBoard = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
-const win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-const picks = [[], []]
-let currentTurn = 'x'
-
-const compare = function (playerPoints, winArray) {
-  const resultArr = []
-
-  for (let i = 0; i < playerPoints.length; i++) {
-    for (let j = 0; j < winArray.length; j++) {
-      if (playerPoints[i] === winArray[j]) {
-        resultArr.push(playerPoints[i])
-      }
-    }
-  }
-  return resultArr.length === 3
-}
-
-const checkWin = function () {
-  if (compare(picks[0], win[0]) === true || compare(picks[0], win[1]) === true ||
-compare(picks[0], win[2]) === true || compare(picks[0], win[3]) === true ||
-compare(picks[0], win[4]) === true || compare(picks[0], win[5]) === true ||
-compare(picks[0], win[6]) === true || compare(picks[0], win[7]) === true) {
-    alert('X wins!')
-  } else if (compare(picks[1], win[0]) === true || compare(picks[1], win[1]) === true ||
-compare(picks[1], win[2]) === true || compare(picks[1], win[3]) === true ||
-compare(picks[1], win[4]) === true || compare(picks[1], win[5]) === true ||
-compare(picks[1], win[6]) === true || compare(picks[1], win[7]) === true) {
-    alert('O wins!')
-  }
-}
-
-const fieldPickLogic = function (fieldId, index) {
-  if (currentTurn === 'x' && gameBoard[index] === undefined) {
-    picks[0].push(index)
-    gameBoard[index] = 'x'
-    $(fieldId).text('x')
-    currentTurn = 'o'
-  } else if (currentTurn === 'o' && gameBoard[index] === undefined) {
-    picks[1].push(index)
-    gameBoard[index] = 'o'
-    $(fieldId).text('o')
-    currentTurn = 'x'
-  } else if (gameBoard[index] !== undefined) {
-    alert('Pick another box plz!')
-  }
-  store.game.cells = gameBoard
-  checkWin()
-  // console.log('X\'s picks: ', picks[0], 'O\'s picks: ', picks[1], 'Whose turn it is now: ', currentTurn)
-  console.log(store.game.cells)
-}
-
-$('#first').on('click', function () { fieldPickLogic('#first', 0) })
-$('#second').on('click', function () { fieldPickLogic('#second', 1) })
-$('#third').on('click', function () { fieldPickLogic('#third', 2) })
-$('#fourth').on('click', function () { fieldPickLogic('#fourth', 3) })
-$('#fifth').on('click', function () { fieldPickLogic('#fifth', 4) })
-$('#sixth').on('click', function () { fieldPickLogic('#sixth', 5) })
-$('#seventh').on('click', function () { fieldPickLogic('#seventh', 6) })
-$('#eighth').on('click', function () { fieldPickLogic('#eighth', 7) })
-$('#ninth').on('click', function () { fieldPickLogic('#ninth', 8) })
